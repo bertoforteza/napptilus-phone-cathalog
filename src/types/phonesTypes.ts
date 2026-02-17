c80bd90859a024bb59c0d41de9eb9269bd62a64e
Berto <bertoforteza87@gmail.com>
2026-02-17 17:58:57 +0100

    Create getPhoneDetails functions and loadPhoneDetails on usePhones hook

--- src/types/phonesTypes.ts
+++ src/types/phonesTypes.ts
@@ -5,3 +5,46 @@ export interface Phone {
   basePrice: number;
   imageUrl: string;
 }
+
+export interface ColorOption {
+  name: string;
+  hexCode: string;
+  imageUrl: string;
+}
+
+export interface SimilarProduct {
+  id: string;
+  brand: string;
+  name: string;
+  basePrice: number;
+  imageUrl: string;
+}
+
+export interface Specs {
+  screen: string;
+  resolution: string;
+  processor: string;
+  mainCamera: string;
+  selfieCamera: string;
+  battery: string;
+  os: string;
+  screenRefreshRate: string;
+}
+
+export interface StorageOption {
+  capacity: string;
+  price: number;
+}
+
+export interface PhoneDetails {
+  id: string;
+  brand: string;
+  name: string;
+  description: string;
+  basePrice: number;
+  rating: number;
+  specs: Specs;
+  colorOptions: ColorOption[];
+  storageOptions: StorageOption[];
+  similarProducts: SimilarProduct[];
+}
